6d3d4cf014a946ed4d154d6a81018557842c536e
Zarph <ramiro@leapcommerce.com>
2013-04-16 17:19:48 -0300

    Initial Commit

---
+++ TumblrSDKExample/TumblrAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TumblrAppDelegate.h
+//  TumblrSDKExample
+//
+//  Created by Ramiro Guerrero on 16/04/13.
+//  Copyright (c) 2013 Ramiro Guerrero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TumblrAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TumblrSDKExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TumblrSDKExample
+//
+//  Created by Ramiro Guerrero on 16/04/13.
+//  Copyright (c) 2013 Ramiro Guerrero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TumblrAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TumblrAppDelegate class]));
+    }
+}
